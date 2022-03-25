3b21e84223bd733c8f7021e443cf731f7b0f2876
LUCAS MOREIRA PACHECO <85808658+lucaslmp2@users.noreply.github.com>
2022-03-25 00:14:51 -0300

    Add files via upload

---
+++ Aula2.java
@@ -0,0 +1,21 @@
+package Aula2;
+
+public class Aula2 {
+	
+	public void soma (int x, int y) {
+		System.out.println(x+y);
+	}
+	
+	public void divisao (float x, float y) {
+		System.out.println(x/y);
+	}
+	
+	public void subtracao (int x, int y) {
+		System.out.println(x-y);
+	}
+	
+	public void multiplicao (int x, int y) {
+		System.out.println(x*y);
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+package Aula2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Aula2 resultado = new Aula2();
+		
+		resultado.soma(0, 10);
+		
+		resultado.subtracao(10,0);
+		
+		resultado.divisao(20, 2);
+		
+		resultado.multiplicao(10, 1);
+
+	}
+
+}
